45479b012bee3fd13e1e18356fcebe2c7f241d2f
omar trigui <omar trigui>
2016-11-27 03:31:48 +0100

    seven

--- app/src/main/java/com/example/omart/projetdevfest/MainActivity.java
+++ app/src/main/java/com/example/omart/projetdevfest/MainActivity.java
@@ -3,11 +3,23 @@ package com.example.omart.projetdevfest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        startActivityForResult(.getInstance()
+                .createSignInIntentBuilder()
+                .setProviders(
+                        AuthUI.FACEBOOK_PROVIDER,
+                        AuthUI.GOOGLE_PROVIDER,
+                        AuthUI.EMAIL_PROVIDER
+                        .build(),1
+                        );
     }
 }
